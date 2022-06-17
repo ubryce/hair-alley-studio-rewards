dfae2bbde0b16b2e366cc68693cfa9a9367823c2
ubryce <bknguyen@ualberta.ca>
2022-06-17 14:10:38 -0600

    Initial Commit

---
+++ hair-alley-studio-rewards/hair_alley_studio_rewardsApp.swift
@@ -0,0 +1,20 @@
+//
+//  hair_alley_studio_rewardsApp.swift
+//  hair-alley-studio-rewards
+//
+//  Created by Bryce Nguyen on 2022-06-17.
+//
+
+import SwiftUI
+
+@main
+struct hair_alley_studio_rewardsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
